d4e7af8a4bed8584db2328f1d1592360ec916f5d
Memoyu <mmy6076@outlook.com>
2021-01-21 17:26:35 +0800

    【Update】修改启动页、添加菜单

--- src/mbill_blazor_admin/Program.cs
+++ src/mbill_blazor_admin/Program.cs
@@ -8,12 +8,14 @@ using Microsoft.Extensions.DependencyInjection;
 namespace mbill_blazor_admin
 {
     public class Program
-    {
+    { 
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+
+            Utils.ThemeColors.Add("#be6868", "mbillcolor");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddAntDesign();
             builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
