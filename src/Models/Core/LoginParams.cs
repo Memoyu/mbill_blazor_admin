aa3aad10d8c1e4aa28cf2e5ec6cbc1ed9230efb6
Memoyu <mmy6076@outlook.com>
2021-03-29 23:14:21 +0800

    【Update】添加登陆页面，调整文件

---
+++ src/Models/Core/LoginParams.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mbill_blazor_admin.Models.Core
+{
+    public class LoginParams
+    {
+        [Required] public string UserName { get; set; }
+
+        [Required] public string Password { get; set; }
+    }
+}
---
+++ src/Pages/Core/Account/Login.razor.cs
@@ -0,0 +1,26 @@
+﻿using mbill_blazor_admin.Models.Core;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mbill_blazor_admin.Pages.Core.Account
+{
+    public partial class Login
+    {
+        private readonly LoginParams _loginModel = new LoginParams();
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        public void HandleSubmit()
+        {
+            if (_loginModel.UserName == "admin" && _loginModel.Password == "ant.design")
+            {
+                NavigationManager.NavigateTo("/");
+                return;
+            }
+
+            if (_loginModel.UserName == "user" && _loginModel.Password == "ant.design") NavigationManager.NavigateTo("/");
+        }
+    }
+}
